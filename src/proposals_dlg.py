ce75a53bdd30013a802cd972cdeb7ee1b6a7ad2d
Bertrand256 <blogin@nullteq.com>
2017-07-18 22:52:34 +0200

    Proposals: dynamic columns

--- src/proposals_dlg.py
+++ src/proposals_dlg.py
@@ -14,16 +14,51 @@ from dashd_intf import DashdIndexException
 from ui import ui_proposals
 
 
-class Proposal(object):
-    def __init__(self):
-        pass
-
-
 class ProposalColumn(object):
     def __init__(self, symbol, caption, visible):
         self.column_symbol = symbol
         self.caption = caption
         self.visible = visible
+
+
+PROPOSAL_COLUMNS = [
+    ProposalColumn('name', 'Name', True),
+    ProposalColumn('payment_start', 'Payment start', True),
+    ProposalColumn('payment_end', 'Payment end', True),
+    ProposalColumn('payment_amount', 'Amount', True),
+    ProposalColumn('yes_count', 'Yes count', True),
+    ProposalColumn('no_count', 'No count', True),
+    ProposalColumn('abstain_count', 'Abstain count', True),
+    ProposalColumn('creation_time', 'Creation time', True),
+    ProposalColumn('url', 'URL', True),
+    ProposalColumn('payment_address', 'Payment address', True),
+    ProposalColumn('hash', 'Hash', True),
+    ProposalColumn('collateral_hash', 'Collateral hash', True),
+    ProposalColumn('fCachedDelete', 'fCachedDelete', True),
+    ProposalColumn('fCachedFunding', 'fCachedFunding', True),
+    ProposalColumn('fCachedEndorsed', 'fCachedEndorsed', True),
+    ProposalColumn('ObjectType', 'ObjectType', True),
+    ProposalColumn('fBlockchainValidity', 'fBlockchainValidity', True),
+    ProposalColumn('IsValidReason', 'IsValidReason', True)
+]
+
+
+class Proposal(object):
+    def __init__(self):
+        pass
+
+    def __getattr__(self, name):
+        for pc in PROPOSAL_COLUMNS:
+            if pc.column_symbol == name:
+                return None
+        raise AttributeError('Attribute "%s" not found inside the Proposal object' % name)
+
+    def __setattr__(self, name, value):
+        for pc in PROPOSAL_COLUMNS:
+            if pc.column_symbol == name:
+                super().__setattr__(name, value)
+                return
+        raise AttributeError('Attribute "%s" not found inside the proposal object' % name)
 
 
 class ProposalsDlg(QDialog, ui_proposals.Ui_ProposalsDlg, wnd_utils.WndUtils):
@@ -33,21 +68,8 @@ class ProposalsDlg(QDialog, ui_proposals.Ui_ProposalsDlg, wnd_utils.WndUtils):
         self.main_wnd = parent
         self.dashd_intf = dashd_intf
         self.proposals = []
+        self.props = []
         self.mn_count = None
-        self.columns = [
-            ProposalColumn('NAME', 'Name', True),
-            ProposalColumn('HASH', 'Hash', False),
-            ProposalColumn('COLLATERAL_HASH', 'Collateral hash', False),
-            ProposalColumn('PAYMENT_START', 'Payment start', True),
-            ProposalColumn('PAYMENT_END', 'Payment end', True),
-            ProposalColumn('PAYMENT_AMOUNT', 'Amount', True),
-            ProposalColumn('YES_COUNT', 'Yes count', True),
-            ProposalColumn('NO_COUNT', 'No count', True),
-            ProposalColumn('ABSTAIN_COUNT', 'Abstain count', True),
-            ProposalColumn('CREATION_TIME', 'Creation time', True),
-            ProposalColumn('URL', 'URL', True),
-            ProposalColumn('PAYMENT_ADDRESS', 'Payment address', False)
-        ]
         self.setupUi()
 
     def setupUi(self):
@@ -57,8 +79,8 @@ class ProposalsDlg(QDialog, ui_proposals.Ui_ProposalsDlg, wnd_utils.WndUtils):
 
             # setup proposals grid
             self.tableWidget.clear()
-            self.tableWidget.setColumnCount(len(self.columns))
-            for idx, col in enumerate(self.columns):
+            self.tableWidget.setColumnCount(len(PROPOSAL_COLUMNS))
+            for idx, col in enumerate(PROPOSAL_COLUMNS):
                 item = QtWidgets.QTableWidgetItem()
                 item.setText(col.caption)
                 self.tableWidget.setHorizontalHeaderItem(idx, item)
@@ -78,13 +100,69 @@ class ProposalsDlg(QDialog, ui_proposals.Ui_ProposalsDlg, wnd_utils.WndUtils):
         selected = len(items) > 0
         self.buttonBox.button(QDialogButtonBox.Ok).setEnabled(selected)
 
+    def column_index_by_name(self, name):
+        """
+        Returns index of a column with a given name.
+        :param name: name of a column
+        :return: index of a column
+        """
+        for idx, pc in enumerate(PROPOSAL_COLUMNS):
+            if pc.column_symbol == name:
+                return idx
+        raise Exception('Invalid proposal column name: ' + name)
+
     def load_proposals_thread(self, ctrl):
+        def find_prop_data(prop_data, level=1):
+            """
+            Find proposal dict inside a list extracted from DataString field
+            """
+            if isinstance(prop_data, list):
+                if len(prop_data) > 2:
+                    logging.warning('len(prop_data) > 2 [level: %d]. prop_data: %s' % (level, json.dumps(prop_data)))
+
+                if len(prop_data) >= 2 and prop_data[0] == 'proposal' and isinstance(prop_data[1], dict):
+                    return prop_data[1]
+                elif len(prop_data) >= 1 and isinstance(prop_data[0], list):
+                    return find_prop_data(prop_data[0], level+1)
+            return None
+
         try:
             if not self.dashd_intf.open():
                 self.errorMsg('Dash daemon not connected')
             else:
                 try:
                     self.proposals = self.dashd_intf.gobject("list", "valid", "proposals")
+
+                    for pro_key in self.proposals:
+                        prop_raw = self.proposals[pro_key]
+
+                        prop_dstr = prop_raw.get("DataString")
+                        prop_data_json = json.loads(prop_dstr)
+                        prop_data = find_prop_data(prop_data_json)
+                        if prop_data is None:
+                            continue
+
+                        prop = Proposal()
+                        prop.name = prop_data['name']
+                        prop.hash = prop_raw['Hash']
+                        prop.collateral_hash = prop_raw['CollateralHash']
+                        prop.payment_start = datetime.datetime.fromtimestamp(int(prop_data['start_epoch']))
+                        prop.payment_end = datetime.datetime.fromtimestamp(int(prop_data['end_epoch']))
+                        prop.payment_amount = float(prop_data['payment_amount'])
+                        prop.yes_count = int(prop_raw['YesCount'])
+                        prop.no_count = int(prop_raw['NoCount'])
+                        prop.abstain_count = int(prop_raw['AbstainCount'])
+                        prop.creation_time = datetime.datetime.fromtimestamp(int(prop_raw["CreationTime"]))
+                        prop.url = prop_data['url']
+                        prop.payment_address = prop_data["payment_address"]
+                        prop.fCachedDelete = prop_raw['fCachedDelete']
+                        prop.fCachedFunding = prop_raw['fCachedFunding']
+                        prop.fCachedEndorsed = prop_raw['fCachedEndorsed']
+                        prop.ObjectType = prop_raw['ObjectType']
+                        prop.fBlockchainValidity = prop_raw['fBlockchainValidity']
+                        prop.IsValidReason = prop_raw['IsValidReason']
+                        self.props.append(prop)
+
                     mns = self.dashd_intf.get_masternodelist('full')
                     self.mn_count = 0
                     statuses = {}
@@ -100,6 +178,7 @@ class ProposalsDlg(QDialog, ui_proposals.Ui_ProposalsDlg, wnd_utils.WndUtils):
                     self.errorMsg(str(e))
 
                 except Exception as e:
+                    logging.exception('Exception while retrieving proposals data.')
                     self.errorMsg('Error occurred while calling getaddressutxos method: ' + str(e))
         except Exception as e:
             pass
@@ -116,18 +195,6 @@ class ProposalsDlg(QDialog, ui_proposals.Ui_ProposalsDlg, wnd_utils.WndUtils):
                 item.setTextAlignment(horz_align)
 
             return item
-
-        """
-        Columns:
-            0: name
-            1: payment start
-            2: payment end 
-            3: payment amount
-            4: yes count
-            5: no count
-            6: abstain count
-            7: url
-        """
 
         """
         'AbsoluteYesCount' = {int} 1233
@@ -183,113 +250,49 @@ class ProposalsDlg(QDialog, ui_proposals.Ui_ProposalsDlg, wnd_utils.WndUtils):
         'fCachedValid' (5856443568) = {bool} True        
         """
 
-        def find_prop_data(prop_data):
-            """
-            Find proposal dict inside list returned in DataString field from RPC node 
-            :param prop_data: 
-            :return: 
-            """
-            if isinstance(prop_data, list):
-                if len(prop_data) >= 2 and prop_data[0] == 'proposal' and isinstance(prop_data[1], dict):
-                    return prop_data[1]
-                elif len(prop_data) >= 1 and isinstance(prop_data[0], list):
-                    return find_prop_data(prop_data[0])
-            return None
-
         try:
             row = 0
 
-            # todo: logging
-            csv = [
-                "proposal_name",
-                "Hash",
-                "fCachedDelete",
-                "fCachedFunding",
-                "ObjectType",
-                "fBlockchainValidity",
-                "fCachedEndorsed",
-                "IsValidReason",
-                "AbstainCount",
-                "CollateralHash",
-                "YesCount",
-                "NoCount",
-                "AbsoluteYesCount",
-                "fCachedValid",
-                "CreationTime",
-                "proposal_url",
-                "proposal_type",
-                "proposal_end_epoch",
-                "proposal_start_epoch",
-                "proposal_payment_address",
-                "proposal_payment_amount"]
-            print('\t'.join(csv))
-
-            for pro_key in self.proposals:
-                prop = self.proposals[pro_key]
+            for prop in self.props:
                 # if prop.get('fCachedFunding', False) or prop.get('fCachedEndorsed', False):
                 #     continue
                 self.tableWidget.insertRow(self.tableWidget.rowCount())
 
-                #todo: while debugging
-                logging.debug('')
-                logging.debug('=========================================================================================')
-                logging.debug(json.dumps(prop))
-
-                dstr = prop.get("DataString")
-                prop_data_json = json.loads(dstr)
-                prop_data = find_prop_data(prop_data_json)
-
-                # todo: while debugging
-                logging.debug('-----------------------------------')
-                logging.debug(json.dumps(prop_data_json))
-
-                if prop_data:
-                    # todo: tempoary logging
-                    csv = [
-                        prop_data.get("name"),
-                        prop.get("Hash"),
-                        prop.get("fCachedDelete"),
-                        prop.get("fCachedFunding"),
-                        prop.get("ObjectType"),
-                        prop.get("fBlockchainValidity"),
-                        prop.get("fCachedEndorsed"),
-                        prop.get("IsValidReason"),
-                        prop.get("AbstainCount"),
-                        prop.get("CollateralHash"),
-                        prop.get("YesCount"),
-                        prop.get("NoCount"),
-                        prop.get("AbsoluteYesCount"),
-                        prop.get("fCachedValid"),
-                        datetime.datetime.fromtimestamp(int(prop.get("CreationTime"))).strftime(DATETIME_FORMAT),
-                        prop_data.get("url"),
-                        prop_data.get("type"),
-                        datetime.datetime.fromtimestamp(int(prop_data['end_epoch'])).strftime(DATE_FORMAT),
-                        datetime.datetime.fromtimestamp(int(prop_data['start_epoch'])).strftime(DATE_FORMAT),
-                        prop_data.get("payment_address"),
-                        prop_data.get("payment_amount")
-                    ]
-                    print('\t'.join([str(r) for r in csv]))
-
-                    # "name" column display as a hyperlink if possible
-                    url = prop_data.get('url', '')
-                    if url:
-                        url_lbl = QtWidgets.QLabel(self.tableWidget)
-                        url_lbl.setText('<a href="%s">%s</a>' % (url, prop_data.get('name', '')))
-                        url_lbl.setOpenExternalLinks(True)
-                        self.tableWidget.setCellWidget(row, 0, url_lbl)
-                    else:
-                        self.tableWidget.setItem(row, 0, prop_data.get('name', ''))
-
-                    dstart = datetime.datetime.fromtimestamp(int(prop_data['start_epoch'])).strftime(DATE_FORMAT)
-                    dend = datetime.datetime.fromtimestamp(int(prop_data['end_epoch'])).strftime(DATE_FORMAT)
-                    self.tableWidget.setItem(row, 1, item(dstart))  # 'Payment start' column
-                    self.tableWidget.setItem(row, 2, item(dend))  # 'Payment end' column
-                    self.tableWidget.setItem(row, 3, item(float(prop_data.get('payment_amount')), Qt.AlignRight))  # 'Payment amount' column
-                    self.tableWidget.setItem(row, 4, item(int(prop.get('YesCount', '')), Qt.AlignRight))  # 'Yes count' column
-                    self.tableWidget.setItem(row, 5, item(int(prop.get('NoCount', '')), Qt.AlignRight))  # 'No count' column
-                    self.tableWidget.setItem(row, 6, item(int(prop.get('AbstainCount', '')), Qt.AlignRight))  # 'Abstain count' column
+                # "name" column display as a hyperlink if possible
+                if prop.url:
+                    url_lbl = QtWidgets.QLabel(self.tableWidget)
+                    url_lbl.setText('<a href="%s">%s</a>' % (prop.url, prop.name))
+                    url_lbl.setOpenExternalLinks(True)
+                    self.tableWidget.setCellWidget(row, self.column_index_by_name('name'), url_lbl)
                 else:
-                    logging.warning("Not found proposal data for %s" % prop.get("Hash"))
+                    self.tableWidget.setItem(row, self.column_index_by_name('name'), prop.name)
+
+                self.tableWidget.setItem(row, self.column_index_by_name('hash'), item(prop.hash))
+                self.tableWidget.setItem(row, self.column_index_by_name('collateral_hash'), item(prop.collateral_hash))
+                self.tableWidget.setItem(row, self.column_index_by_name('payment_start'), item(prop.payment_start))
+                self.tableWidget.setItem(row, self.column_index_by_name('payment_end'), item(prop.payment_end))
+                self.tableWidget.setItem(row, self.column_index_by_name('payment_amount'), item(prop.payment_amount, Qt.AlignRight))
+                self.tableWidget.setItem(row, self.column_index_by_name('yes_count'), item(prop.yes_count, Qt.AlignRight))
+                self.tableWidget.setItem(row, self.column_index_by_name('no_count'), item(prop.no_count, Qt.AlignRight))
+                self.tableWidget.setItem(row, self.column_index_by_name('abstain_count'), item(prop.abstain_count, Qt.AlignRight))
+                self.tableWidget.setItem(row, self.column_index_by_name('creation_time'), item(prop.creation_time))
+
+                if prop.url:
+                    url_lbl = QtWidgets.QLabel(self.tableWidget)
+                    url_lbl.setText('<a href="%s">%s</a>' % (prop.url, prop.url))
+                    url_lbl.setOpenExternalLinks(True)
+                    self.tableWidget.setCellWidget(row, self.column_index_by_name('url'), url_lbl)
+                else:
+                    self.tableWidget.setItem(row, self.column_index_by_name('url'), prop.url)
+
+                self.tableWidget.setItem(row, self.column_index_by_name('payment_address'), item(prop.payment_address))
+                self.tableWidget.setItem(row, self.column_index_by_name('fCachedDelete'), item(prop.fCachedDelete))
+                self.tableWidget.setItem(row, self.column_index_by_name('fCachedFunding'), item(prop.fCachedFunding))
+                self.tableWidget.setItem(row, self.column_index_by_name('fCachedEndorsed'), item(prop.fCachedEndorsed))
+                self.tableWidget.setItem(row, self.column_index_by_name('ObjectType'), item(prop.ObjectType))
+                self.tableWidget.setItem(row, self.column_index_by_name('fBlockchainValidity'), item(prop.fBlockchainValidity))
+                self.tableWidget.setItem(row, self.column_index_by_name('IsValidReason'), item(prop.IsValidReason))
+
                 row += 1
 
             self.tableWidget.resizeColumnsToContents()
