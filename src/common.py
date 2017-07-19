3bee7653563aaa0500cf31f4263e5d9941f236b5
Bertrand256 <b256@protonmail.com>
2017-07-19 22:06:21 +0200

    Proposals voting: added dynamic grid columns, presenting voting results, reading voting results from a Dash daemon

---
+++ src/common.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Author: Bertrand256
+# Created on: 2017-07
+
+class AttrsProtected(object):
+    """
+    Class for protecting of attribute definition to only inside of a constructor.
+    """
+    def __init__(self):
+        self.__allow_attr_definition = True
+
+    def set_attr_protection(self):
+        """
+        Method to be called at the end of child class constructor. It enables attribute
+        defition protection - each attempt of creating attribute after this call will
+        end up with an error.
+        """
+        self.__allow_attr_definition = False
+
+    def __setattr__(self, name, value):
+        if name == '_AttrsProtected__allow_attr_definition' or self.__allow_attr_definition or hasattr(self, name):
+            super().__setattr__(name, value)
+        else:
+            raise AttributeError('Attribute definition protection for class "%s". Attribute name: "%s"' % (self.__class__.__name__, name))
