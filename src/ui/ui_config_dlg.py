7324f20e972c0d234385cd4d3dc2b4a7c3f38be8
Bertrand256 <b256@protonmail.com>
2017-08-24 20:50:56 +0200

    Commit of autogenerated UI files.

--- src/ui/ui_config_dlg.py
+++ src/ui/ui_config_dlg.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file '/Users/blogin/PycharmProjects/DMT-git/src/ui/ui_config_dlg.ui'
 #
-# Created by: PyQt5 UI code generator 5.8.2
+# Created by: PyQt5 UI code generator 5.9
 #
 # WARNING! All changes made in this file will be lost!
 
