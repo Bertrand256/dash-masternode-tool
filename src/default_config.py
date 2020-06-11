5fec807e95b4ce4ff04dab0cd0683e0314aeae0d
Andrey <andrey@zcoin.io>
2020-06-11 15:37:59 +0400

    Default config

--- src/default_config.py
+++ src/default_config.py
@@ -1,4 +1,32 @@
 
 # default "public" connections for RPC proxy
 dashd_default_connections = [
+    {
+        "use_ssh_tunnel": False,
+        "host": "xzc32615.allnodes.me",
+        "port": "443",
+        "username": "ZtUser",
+        "password": "67414141414142653466453577715276767554657a686e4b35474e304b48527879696b3634534d32764d6c3549486a4c6f53465f7333417168547235612d3945656f2d53327a457666454639667256583378784153686363434631567335574a47773d3d",
+        "use_ssl": True,
+        "rpc_encryption_pubkey":"",
+    },
+    {
+        "use_ssh_tunnel": False,
+        "host": "xzc22191.allnodes.me",
+        "port": "443",
+        "username": "ZtUser",
+        "password": "67414141414142653466453577715276767554657a686e4b35474e304b48527879696b3634534d32764d6c3549486a4c6f53465f7333417168547235612d3945656f2d53327a457666454639667256583378784153686363434631567335574a47773d3d",
+        "use_ssl": True,
+        "rpc_encryption_pubkey":"",
+    },
+    {
+        "use_ssh_tunnel": False,
+        "host": "test-ztnode.zcoin.io",
+        "port": "443",
+        "username": "ZtUser",
+        "password": "67414141414142653466453577715276767554657a686e4b35474e304b48527879696b3634534d32764d6c3549486a4c6f53465f7333417168547235612d3945656f2d53327a457666454639667256583378784153686363434631567335574a47773d3d",
+        "use_ssl": True,
+        "testnet": True,
+        "rpc_encryption_pubkey": ""
+    }
 ]
